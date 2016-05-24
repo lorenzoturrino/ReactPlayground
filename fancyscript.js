c572866b4324a18f3dfa0613440df8376da78c78
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-05-24 16:18:28 +0100

    some rendering, nesting elements

--- fancyscript.js
+++ fancyscript.js
@@ -1,4 +1,42 @@
 ReactDOM.render(
-        <h1>Hello,xfgmnxg world!</h1>,
+        <h1>Hello, world!</h1>,
         document.getElementById('example')
       );
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox and these are my minions
+          < CommentList />
+          < CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
